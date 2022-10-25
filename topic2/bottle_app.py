8e0014a2f1b08ab03a7eaf996b561be532fddb99
manasaperavali <peravalimanasa15@gmail.com>
2022-10-25 23:04:37 +0000

    crud operations

---
+++ topic2/bottle_app.py
@@ -0,0 +1,43 @@
+from bottle import default_app, route, get, post, template, request, redirect
+
+import database
+
+@route('/')
+def get_index():
+    redirect('/list')
+
+@route('/hi')
+def get_hi():
+    return "hi people"
+
+@route('/list')
+def get_list():
+    return "Kaushik Thumma"
+
+@post('/add')
+def post_add():
+    description = request.forms.get("description")
+    database.add_item(description)
+    redirect('/list')
+
+@route("/delete/<id>")
+def get_delete(id):
+    database.delete_item(id)
+    redirect('/list')
+
+@get("/edit/<id>")
+def get_edit(id):
+    items = database.get_items(id)
+    if len(items) != 1:
+        redirect('/list')
+    item_id, description = items[0]['id'], items[0]['desc']
+    assert item_id == int(id)
+    return template("edit_item.tpl", id=id, description=description)
+
+@post("/edit/<id>")
+def post_edit(id):
+    description = request.forms.get("description")
+    database.update_item(id, description)
+    redirect('/list')
+
+application = default_app()
